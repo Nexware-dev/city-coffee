a2d2d3e6df191438af45b91fad7c14b8277c1175
pataspatek <patrikpatek3@gmail.com>
2023-05-03 15:09:23 +0200

    UserList modified - Link overlaps each div

--- frontend/src/components/UserList.jsx
+++ frontend/src/components/UserList.jsx
@@ -3,16 +3,14 @@ import { Link } from "react-router-dom";
 function UserList({data}) {
 
     return (
-        <div>
+        <div style={{padding: "10px 150px"}}>
             {data.map((item) => (
-                <div style={{width: "200px", border: "1px solid black", margin: "auto"}}>
-                    <Link to={`/user_detail/${item.id}`} key={item.id}>
-                        <div style={{textAlign: "center"}}>
-                            <h2>{item.name} {item.surname}</h2>
-                            <p>{item.email}</p>
-                        </div>
-                    </Link>
-                </div>
+                <Link to={`/user_detail/${item.id}`} key={item.id}>
+                    <div style={{border: "1px solid black", textAlign: "center", margin: "10px"}}>
+                        <h2>{item.name} {item.surname}</h2>
+                        <p>{item.email}</p>
+                    </div>
+                </Link>
                 ))}
         </div>
     );
