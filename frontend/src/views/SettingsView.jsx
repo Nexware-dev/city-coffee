159ca053b2f77cfc0957f0431b0c27afb05807c9
pataspatek <patrikpatek3@gmail.com>
2023-05-05 17:26:20 +0200

    Test

--- frontend/src/views/SettingsView.jsx
+++ frontend/src/views/SettingsView.jsx
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react";
+
 function SettingsView() {
+
+    const [test, setTest] = useState("placeholder") 
+
+    useEffect(() => {
+        fetch("/")
+        .then(response => response.json())
+        .then(data => setTest(data))
+    }, [])
+
     return (
-        <h1>Placeholder for settings page</h1>
+        <h1>{test}</h1>
     );
 }
 
