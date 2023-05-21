80a89d953fc36d8e8a4026e9a01c87c738998c34
pataspatek <patrikpatek3@gmail.com>
2023-05-21 14:01:06 +0200

    Add comments

--- frontend/src/components/CoffeeStamps.jsx
+++ frontend/src/components/CoffeeStamps.jsx
@@ -8,6 +8,7 @@ function CoffeeStamps({ count, setCount, user_id }) {
   const [markedStamps, setMarkedStamps] = useState([])
 
   useEffect(() => {
+    // Initialize marked stamps based on the count
     const initialStamps = Array.from({ length: COFFEE_NUMBER_REQUIRED })
       .map((_, index) => index)
       .filter((item) => item < count);
@@ -15,16 +16,20 @@ function CoffeeStamps({ count, setCount, user_id }) {
   }, [count])
 
   const handleClick = (index) => {
+    // Toggle the presence of a value 'index' in an array 'markedStamps'
     const newStamps = markedStamps.includes(index) ? markedStamps.filter((i) => i !== index) : [...markedStamps, index];
     setMarkedStamps(newStamps);
 
+    // Update the count
     const newCounter = count + (markedStamps.includes(index) ? -1 : 1);
     setCount(newCounter);
 
+    // Update the counter on the server
     handleCounterUpdate(newCounter);
   }
 
   const handleCounterUpdate = (newCounter) => {
+    // Send a request to update the counter on the server
     fetch("http://localhost:5000/update_counter", {
       method: "POST",
       headers: {
