f839821ce4735ef68da7fa308a37492c6edcf1b8
pataspatek <patrikpatek3@gmail.com>
2023-06-25 16:44:20 +0200

    SearchBar style completed

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,18 +1,21 @@
 function SearchBar({search, onSearch}) {
 
    return (
-    <div className="search-bar-container">
-        <label htmlFor="search" className="search-bar-label">
-            Search:
-        </label>
-        <input 
-            id="search" 
-            type="text"
-            className="search-bar-input" 
-            value={search}
-            onChange={onSearch}
-            placeholder="Enter search query"
-        />
+    <div>
+        <div className="search-bar-container">
+            <label htmlFor="search" className="search-bar-label">
+                Search:
+            </label>
+            <input 
+                id="search" 
+                type="text"
+                className="search-bar-input" 
+                value={search}
+                onChange={onSearch}
+                placeholder="Enter search query"
+            />
+        </div>
+        <div className="search-bar-line"></div>
     </div>
    );
 }
